f29f2354d3a02d8030756b7520980e1bafe09f29
Layo Kaminski <layokaminski@gmail.com>
2020-11-25 20:08:37 -0300

    Resoluçao exercicio 10

---
+++ bloco_4/dia_1/exercise10.js
@@ -0,0 +1,7 @@
+let custoProduto = 200
+let valorVenda = 400;
+let valorCustoTotal = custoProduto * 0.20;
+let lucro = valorVenda - valorCustoTotal;
+let vendas = lucro * 1000
+
+console.log('A empresa irá vender ' + vendas + ' produtos');
