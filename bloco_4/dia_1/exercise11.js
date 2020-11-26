85090748d4dbe570843b6c9c1f7b0c2f4cbd7d48
Layo Kaminski <layokaminski@gmail.com>
2020-11-26 13:58:53 -0300

    Resoluçao do exercicio 11

--- bloco_4/dia_1/exercise11.js
+++ bloco_4/dia_1/exercise11.js
@@ -1,3 +1,36 @@
-let salario = (1500 >= 2500) * 0.08
+let salario = 3000
+let salarioInss = 0
+let salarioLiquido;
 
-console.log(salario);
+if (salario <= 1556.94) {
+    salarioInss = salario - (salario * 0.08);
+}
+else if (salario > 1556.94 &&  salario <= 2594.92) {
+    salarioInss = salario - (salario * 0.09);
+}
+else if (salario > 2594.92 &&  salario <= 5189.82) {
+    salarioInss = salario - (salario * 0.11);
+}
+else if (salario > 5189.82) {
+    salarioInss = salario - 570.88
+}
+
+let salarioCalculado = salarioInss
+
+if (salarioCalculado < 1903.98) {
+    salarioLiquido;
+}
+else if (salarioCalculado > 1903.98 &&  salarioCalculado <= 2826.65) {
+    salarioLiquido = salarioCalculado - (salarioCalculado * 0.075 - 142.8);
+}
+else if (salarioCalculado > 2826.65 &&  salarioCalculado <= 3751.05) {
+    salarioLiquido = salarioCalculado - (salarioCalculado * 0.15 - 354.80);
+}
+else if (salarioCalculado > 3751.05 &&  salarioCalculado <= 4664.68) {
+    salarioLiquido = salarioCalculado - (salarioCalculado * 0.225 - 636.13);
+}
+else if (salarioCalculado > 4664.68) {
+    salarioLiquido = salarioCalculado - (salarioCalculado * 0.275 - 869.36);
+}
+
+console.log(salarioLiquido);
