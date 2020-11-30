f90344f2ed7bb49bef17c524bfaa06d575e5ba79
Layo Kaminski <layokaminski@gmail.com>
2020-11-30 15:52:05 -0300

    Refatorando exercicio 1 do 4.1

---
+++ bloco_4/dia_4/exercise3.js
@@ -0,0 +1,25 @@
+function sum(a, b) {
+    return a + b
+}
+
+function subtraction(a, b) {
+    return a - b
+}
+
+function multiplication(a, b) {
+    return a * b
+}
+
+function division(a, b) {
+    return a / b
+}
+
+function module(a, b) {
+    return a % b
+}
+
+console.log(sum(3, 2));
+console.log(subtraction(3, 2));
+console.log(multiplication(3, 2));
+console.log(division(3, 2));
+console.log(module(3, 2));
