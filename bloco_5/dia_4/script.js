96c663e572aa6f7661dd3f86a2762511954a96c1
Layo Kaminski <layokaminski@gmail.com>
2020-12-07 16:08:59 -0300

    background color e font color executados

---
+++ bloco_5/dia_4/script.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    function changeBackgroundColor (color) {
+        let content = document.getElementById('text')
+        content.style.backgroundColor = color
+        localStorage.setItem('backgroundColor', color)
+    }
+    let buttonBackGround = document.querySelectorAll('#background-color > button')
+    for (let index = 0; index < buttonBackGround.length; index += 1) {
+        buttonBackGround[index].addEventListener('click', function(event) {
+            changeBackgroundColor(event.target.innerHTML)
+        })
+    }
+    function changeTextColor (color) {
+        let content = document.querySelectorAll('.paragraph');
+        for (let index = 0; index < content.length; index += 1) {
+        content[index].style.color = color
+        }
+        localStorage.setItem('fontColor', color)
+    }
+    let buttonFontColor = document.querySelectorAll('#font-color > button')
+    for (let index = 0; index < buttonFontColor.length; index += 1) {
+        buttonFontColor[index].addEventListener('click', function(event) {
+            changeTextColor(event.target.innerHTML)
+        })
+    }
+}
