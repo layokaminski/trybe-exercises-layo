e29bbd27885d7b55174a6303e95877fb3e4941a1
Layo Kaminski <layokaminski@gmail.com>
2021-01-05 18:09:23 -0300

    Resoluçao exercicio 2

--- bloco_6/dia_2/script.js
+++ bloco_6/dia_2/script.js
@@ -11,26 +11,23 @@ function createOptions(estadosBrasileiros) {
 }
 createOptions(estadosBrasileiros);
 
-function validarData(data) {
-  if (data.indexOf('-') === 2 || data.indexOf('-') === 5) {
-    const dia = data.substr(0, 2);
-    const mes = data.substr(3, 2);
-    const ano = data.substr(6, 4);
-    if ((dia > 0 && dia < 31) && (mes > 0 && mes <= 12) && (ano > 0 && ano.length === 4)) {
-      return true;
-    }
+var picker = new Pikaday({
+  field: document.getElementById('data'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+      // you should do formatting based on the passed format,
+      // but we will just return 'D/M/YYYY' for simplicity
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+      // dateString is the result of `toString` method
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
   }
-  return false;
-}
-
-function checkData() {
-  const inputData = document.querySelector('#data');
-  let data = inputData.value;
-  const userData = validateData(data);
-  if (userData && data.length) {
-    inputData.value = '';
-    alert('invalida');
-    return false;
-  }
-  return userData;
-}
+});
