ae2b7ea548f3e1f5ca6c97c05f167f76a1a86a94
Layo Kaminski <layokaminski@gmail.com>
2021-01-12 20:31:43 -0300

    Exercicio 4 - Parte 2 (Nao finalizado)

--- bloco_7/dia_1/exercise-part2.js
+++ bloco_7/dia_1/exercise-part2.js
@@ -1,4 +1,4 @@
-// Crie uma função que receba um número e retorne seu fatorial.
+// 1. Crie uma função que receba um número e retorne seu fatorial.
 
 const factorial = num => {
   let result = 1
@@ -11,26 +11,27 @@ const factorial = num => {
 
 console.log(factorial(7));
 
-// Crie uma função que receba uma frase e retorne qual a maior palavra.
+// 2. Crie uma função que receba uma frase e retorne qual a maior palavra.
 
 const biggerLetter = frase => {
   const arrayFrase = frase.split(' ');
+  console.log(arrayFrase)
   let maior = arrayFrase[0];
   for (let index = 0; index < arrayFrase.length; index += 1) {
     if (arrayFrase[index].length > maior.length) {
-       maior = arrayFrase[index];
-    } 
+      maior = arrayFrase[index];
+    }
   }
   return maior;
 }
 
 console.log(biggerLetter("Antônio foi no banheiro e não sabemos o que aconteceu"))
 
-/* Crie uma página que contenha:
+/* 3. Crie uma página que contenha:
 
 Um botão ao qual será associado um event listener;
 Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
-Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada. */
+Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada. 
 
 const button = document.querySelector('button');
 let num = document.querySelector('span');
@@ -38,4 +39,30 @@ let clickCount = 0
 
 button.addEventListener('click', () => {
   num.innerText = clickCount += 1;
-})
+}) */
+
+/* 4. Crie um código JavaScript com a seguinte especificação:
+
+Função 1: Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string.
+
+Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string. */
+
+const changeString = string => {
+  let change = string.replace('x', 'Layo')
+  return change
+}
+
+console.log(changeString('Tryber x aqui!'));
+
+const skills = ['HTML', 'CSS', 'ES6', 'Mobile First', 'JavaScript']
+
+const countainerSkills = (string, array) => {
+  array.sort((a, b) => a - b);
+
+  return `${string} Minhas cinco principais habilidades são:
+    ${array[0]}
+    ${array[1]}
+    #goTrybe`
+}
+
+console.log(countainerSkills(changeString('Tryber x aqui!'), skills));
