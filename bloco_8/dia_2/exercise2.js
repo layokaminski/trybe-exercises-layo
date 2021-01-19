3b44d90fa0ee6bc6f6fe28eea4a3b1de5ddca282
Layo Kaminski <layokaminski@gmail.com>
2021-01-19 16:04:40 -0300

    Resoluçao exercicio 2

--- bloco_8/dia_2/exercise2.js
+++ bloco_8/dia_2/exercise2.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-// 1. Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+// 2. Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
 
 const books = [
   {
@@ -66,17 +66,38 @@ const books = [
 ];
 
 const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft'
+  {
+    age: 31,
+    author: 'Isaac Asimov'
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft'
+  },
+  {
+    age: 39,
+    author: 'Stephen King'
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin'
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert'
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien'
+  }
 ];
 
-function formatedBookNames() {
+function nameAndAge() {
   // escreva seu código aqui
-  return books.map(book => `${book.name} - ${book.genre} - ${book.author.name}`)
-}
+  return books.map(book => { 
+  return { author: book.author.name, age: (book.releaseYear - book.author.birthYear) 
+  }}).sort((value1, value2) => value1.age - value2.age);
+};
 
-assert.deepStrictEqual(formatedBookNames(), expectedResult);
+
+assert.deepStrictEqual(nameAndAge(), expectedResult);
