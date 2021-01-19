2c47711761a2b0478c8b29fed9e01bf9af844f97
Layo Kaminski <layokaminski@gmail.com>
2021-01-19 15:52:14 -0300

    Resoluçao exercicio 1

--- bloco_8/dia_2/pratice2.js
+++ bloco_8/dia_2/pratice2.js
@@ -24,3 +24,12 @@ const personsMap = [
 const fullNamesMap = personsMap.map((person) => `${person.firstName} ${person.lastName}`);
 
 console.log(fullNamesMap); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+// Suponha que é preciso transformar todos os números em negativos e passa-lós para um array novo.
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map(number => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
