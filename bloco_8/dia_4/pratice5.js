62746afd0e01dfb92a6cc300ce9e0bbc373022f1
Layo Kaminski <layokaminski@gmail.com>
2021-01-21 15:53:51 -0300

    Praticando com Object property shorthand

--- bloco_8/dia_4/pratice5.js
+++ bloco_8/dia_4/pratice5.js
@@ -1,20 +1,15 @@
 // Praticando com default destructuring
 
-const nationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`
-
 const person = {
-  firstName: "João",
-  lastName: "Jr II"
-};
-
-
-const otherPerson = {
-  firstName: "Ivan",
-  lastName: "Ivanovich",
-  nationality: "Russian"
+  name: "João",
+  lastName: "Jr",
+  age: 34
 };
 
 const { nationality = "Brazilian" } = person;
+console.log(nationality); // Brazilian
 
-console.log(nationality(otherPerson)); // Ivan is Russian
-console.log(nationality(person));
+const position2d = [1.0, 2.0];
+const [x, y, z = 0] = position2d;
+
+console.log(z); // 0
