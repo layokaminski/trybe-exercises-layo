e1e11ff6b18709aef93ed71a5874f6c114a2dcca
Layo Kaminski <layokaminski@gmail.com>
2021-01-21 18:11:09 -0300

    Resoluçao exercicio 3

--- bloco_8/dia_4/pratice5.js
+++ bloco_8/dia_4/pratice5.js
@@ -1,15 +1,23 @@
 // Praticando com default destructuring
 
-const person = {
-  name: "João",
-  lastName: "Jr",
-  age: 34
-};
-
-const { nationality = "Brazilian" } = person;
-console.log(nationality); // Brazilian
-
 const position2d = [1.0, 2.0];
 const [x, y, z = 0] = position2d;
 
 console.log(z); // 0
+
+
+const nationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+    firstName: "João",
+    lastName: "Jr II"
+};
+
+const otherPerson = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+};
+
+console.log(nationality(otherPerson)); // Ivan is Russian
+console.log(nationality(person));
