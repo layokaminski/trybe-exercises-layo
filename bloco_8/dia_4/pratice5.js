455992a3a23904f78f534980a688ebf3226ff78e
Layo Kaminski <layokaminski@gmail.com>
2021-01-21 15:40:47 -0300

    Praticando com default destructuring

---
+++ bloco_8/dia_4/pratice5.js
@@ -0,0 +1,20 @@
+// Praticando com default destructuring
+
+const nationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`
+
+const person = {
+  firstName: "João",
+  lastName: "Jr II"
+};
+
+
+const otherPerson = {
+  firstName: "Ivan",
+  lastName: "Ivanovich",
+  nationality: "Russian"
+};
+
+const { nationality = "Brazilian" } = person;
+
+console.log(nationality(otherPerson)); // Ivan is Russian
+console.log(nationality(person));
