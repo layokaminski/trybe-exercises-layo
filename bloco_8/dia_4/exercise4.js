762b8d931bdf85dcfc654f751f034f7e6448d022
Layo Kaminski <layokaminski@gmail.com>
2021-01-21 18:39:35 -0300

    Resoluçao exercicio 4

---
+++ bloco_8/dia_4/exercise4.js
@@ -0,0 +1,42 @@
+const assert = require('assert')
+
+// 4. Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+// escreva filterPeople abaixo
+
+function filterPeople(array) {
+  return array.filter(({ bornIn, nationality }) => bornIn > 1900 && bornIn < 2000 && nationality === 'Australian');
+}
+
+const filteredPeople = filterPeople(people)
+
+assert.deepStrictEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepStrictEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
