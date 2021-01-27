7480a425a3a84ac603daae31ac023463d047c343
Layo Kaminski <layokaminski@gmail.com>
2021-01-27 12:22:35 -0300

    Resoluçao exercicio de fixaçao 1 - callbacks

--- bloco_9/dia_1/exercise-fixation1.js
+++ bloco_9/dia_1/exercise-fixation1.js
@@ -1,38 +1,19 @@
-/* const assert = require('assert');
-
-const pushNumber = (list, number) => list.push(number);
-
-let numbers = [];
-
-pushNumber(numbers, 1);
-pushNumber(numbers, 2);
-pushNumber(numbers, 3);
-
-assert.deepStrictEqual(numbers, [1, 2, 3]); */
-
-/* const assert = require('assert');
-
-const pushNumber = (list, number) => list.push(number);
-
-let numbers = [];
-
-setTimeout(() => pushNumber(numbers, 1), 3000);
-pushNumber(numbers, 2);
-pushNumber(numbers, 3);
-
-assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha */
-
 const assert = require('assert');
 
-const pushNumber = (list, number) => {
-  list.push(number);
-  console.log(list);
+// No código abaixo você tem a função getUser , que retorna uma pessoa qualquer. Complete a função getUser de forma que ela receba uma outra função como parâmetro para que possa realizar as operações abaixo sobre a pessoa retornada.
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
 };
 
-let numbers = [];
 
-setTimeout(() => pushNumber(numbers, 1), 3000);
-pushNumber(numbers, 2);
-pushNumber(numbers, 3);
-
-setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
--- bloco_9/dia_1/pratice2.js
+++ bloco_9/dia_1/pratice2.js
@@ -1,35 +1,38 @@
-const myExpenses = [
-  {
-    gym: 99,
-  },
-  {
-    ifood: 200,
-  },
-  {
-    phone: 60,
-  },
-  {
-    internet: 100,
-  },
-];
+/* const assert = require('assert');
 
-const myIncome = 1000;
+const pushNumber = (list, number) => list.push(number);
 
-const monthlyBudget = (myIncome, myExpenses, callback) => {
+let numbers = [];
 
-  const totalExpenses = callback(myExpenses);
-  const totalAfterExpenses = myIncome - totalExpenses;
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
 
-  console.log(`Balanço do mês:
-    Recebido: R$${myIncome},00
-    Gasto: R$${totalExpenses},00
-    Saldo: R$${totalAfterExpenses},00 `);
+assert.deepStrictEqual(numbers, [1, 2, 3]); */
+
+/* const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha */
+
+const assert = require('assert');
+
+const pushNumber = (list, number) => {
+  list.push(number);
+  console.log(list);
 };
 
-const handleExpenses = myExpenses => {
-  const eachValue = myExpenses.map((item) => Object.values(item)[0]);
-  const totalExpense = eachValue.reduce((acc, curr) => acc + curr , 0);
-  return totalExpense;
-};
+let numbers = [];
 
-monthlyBudget(myIncome, myExpenses, handleExpenses);
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
