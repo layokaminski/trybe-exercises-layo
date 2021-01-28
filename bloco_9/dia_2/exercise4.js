12a6be3435bc625751a7c4d615e84f18a910a4f3
Layo Kaminski <layokaminski@gmail.com>
2021-01-28 19:40:19 -0300

    Resoluçao exercise 4

---
+++ bloco_9/dia_2/exercise4.js
@@ -0,0 +1,12 @@
+const promise = new Promise((resolve, reject) => {
+  const result = Array.from({ length: 10 }, () => 
+  Math.pow(Math.floor(Math.random(1) * 50), 2))
+  .reduce((acumulator, current) => acumulator + current);
+
+  if (result > 8000) {
+    reject(result)
+  }
+  resolve(result);
+})
+  .then(result => console.log([2, 3, 5, 10].map(number => Math.round(result / number))))
+  .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'))
