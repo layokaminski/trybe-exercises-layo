8cbc12c2e06eb455f17554e1c881038addc3e81a
Layo Kaminski <layokaminski@gmail.com>
2021-02-03 18:16:19 -0300

    Resoluçao exercise4 - Part 1

---
+++ bloco_10/dia_1/exercises/exercise4-part1.test.js
@@ -0,0 +1,19 @@
+const myFizzBuzz = require('./exercise4-part1');
+
+describe('Requisitos - Exercício 4', () => {
+  it ('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+  });
+  it ('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(3)).toBe('fizz');
+  });
+  it ('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(5)).toBe('buzz');
+  });
+  it ('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(8)).toBe(8);
+  });
+  it ('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz('string')).toBe(false);
+  });
+});
