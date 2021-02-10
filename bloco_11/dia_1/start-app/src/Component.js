07705a248e5416e0aa4c6f23492beb97f73e5637
Layo Kaminski <layokaminski@gmail.com>
2021-02-10 14:54:25 -0300

    Primeira iteraçao com React

---
+++ bloco_11/dia_1/start-app/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Layo Kaminski</h1>
+        <p>Um cara legal que está contente com todas as coisas novas que está aprendendo diariamente</p>
+      </div>
+    )
+  }
+}
+
+export default Test;
