f1714482bf0433e7aff7b47615d2dac1ea0a5157
Layo Kaminski <layokaminski@gmail.com>
2021-02-11 16:18:30 -0300

    Resoluçao do warning com key props

--- bloco_11/dia_2/fixation-exercises-11-2/src/App.js
+++ bloco_11/dia_2/fixation-exercises-11-2/src/App.js
@@ -5,31 +5,32 @@ import Order from './Order';
 
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
+    const itens = [
+      {
+        id: 77,
+        user: "cena@gmail.com",
+        product: "Monster 500mL",
+        price: {
+          value: 9.99,
+          currency: "dollars"
+        }
+      },
+      {
+        id: 102,
+        user: "cena@gmail.com",
+        product: "Razer Headphone",
+        price: {
+          value: 99.99,
+          currency: "dollars"
+        }
       }
-    };
 
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
+    ];
 
     return (
       <div className="App">
         <h1> Orders recently created </h1>
-         <Order order={headphone} />
-         <Order order={energyDrink} />
+        {itens.map((item, index) => <Order key={index} order={item}/>)}
       </div>
     );
   }
--- bloco_11/dia_2/fixation-exercises-11-2/src/Order.js
+++ bloco_11/dia_2/fixation-exercises-11-2/src/Order.js
@@ -4,10 +4,11 @@ import React from 'react';
 class Order extends React.Component {
   render() {
     const { user, product, price } = this.props.order;
+    
 
     return (
       <div className="order">
-        <p> {user} bought {product} for {price.value} {price.currency} </p>
+        <p>{user} bought {product} for {price.value} {price.currency} </p>
       </div>
     );
   }
