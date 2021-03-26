33e81a7cea6639dbba895201e1dc5694e84babd4
Layo Kaminski <layokaminski@gmail.com>
2021-03-26 19:06:38 -0300

    Implementação de novos testes para fixação

--- bloco_16/dia_5/test-react-redux/src/App.test.js
+++ bloco_16/dia_5/test-react-redux/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { cleanup } from '@testing-library/react';
 import App from './App';
 import renderWithRedux from './tests/renderWithRedux';
+import userEvent from '@testing-library/user-event';
 
 describe('testing clicks', () => {
   beforeEach(cleanup);
@@ -17,5 +18,22 @@ describe('testing clicks', () => {
     const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
 
     expect(queryByText('5')).toBeInTheDocument();
+  });
+
+  it('increment the value of clicks', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    userEvent.click(buttonAdicionar);
+    userEvent.click(buttonAdicionar);
+
+    expect(queryByText('2')).toBeInTheDocument();
+  });
+
+  it('increment the value of counter', () => {
+    const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 10 }}});
+
+    expect(queryByText('10')).toBeInTheDocument();
   });
 });
