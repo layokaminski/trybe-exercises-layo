84d877c6b3bc4de3ece071f88c14ab8fe133574f
Layo Kaminski <layokaminski@gmail.com>
2021-06-08 19:38:18 -0300

    Exercicio 12

---
+++ bloco_24/dia_2/exercicio12.js
@@ -0,0 +1,6 @@
+db.movies.updateOne(
+  { title: "Batman", "cast.character": "Batman" },
+  { $push: { "cast.$.actor": 
+  { $each: [ "Michael Keaton" , "Val Kilmer", "George Clooney" ] 
+  } } 
+});
