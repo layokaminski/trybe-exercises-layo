12949751366557f0434d0fe14c8c4aac37303ca5
Layo Kaminski <layokaminski@gmail.com>
2021-06-09 18:48:43 -0300

    Exercicio 16 - Ajuste com a descrição correta

--- bloco_24/dia_3/exercicio16.js
+++ bloco_24/dia_3/exercicio16.js
@@ -2,7 +2,7 @@ db.movies.updateOne(
   { title: "Godzilla" },
   {
     $set: {
-      description: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation."
+      description: "The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity."
     }
   }
 );
