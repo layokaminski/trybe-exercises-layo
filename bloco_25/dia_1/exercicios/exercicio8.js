695b17642712c0cd5ac397e6a55a03910668b56b
Layo Kaminski <layokaminski@gmail.com>
2021-06-11 19:09:20 -0300

    Exercicio 8

---
+++ bloco_25/dia_1/exercicios/exercicio8.js
@@ -0,0 +1,21 @@
+db.vendas.aggregate([
+  {
+    $match: {
+      status: { $in: ["ENTREGUE", "EM SEPARACAO"] }
+    },
+  },
+  {
+    $group: {
+      _id: "$clienteId",
+      valorTotal: {
+        $sum: "$valorTotal",
+      },
+    },
+  },
+  {
+    $sort: { valorTotal: -1 },
+  },
+  {
+    $limit: 5,
+  }
+]);
