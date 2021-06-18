4d49e34f89c65ecd4e87aa2b234b8f1fbad00217
Layo Kaminski <layokaminski@gmail.com>
2021-06-18 15:58:30 -0300

    Exercicios 3 e 4

--- bloco_26/dia_1/imc.js
+++ bloco_26/dia_1/imc.js
@@ -1,9 +1,10 @@
-const peso = 70;
-const altura = 1.65;
+const readline = require('readline-sync');
+
+const peso = readline.questionInt('Qual seu peso? ');;
+const altura = readline.questionFloat('Qual sua altura? ');;;
 
 function calcultedIMC(peso, altura) {
-  const IMC = Math.round(peso / (altura * 2));
-
+  const IMC = Math.round(peso / Math.pow(altura, 2));
   return IMC;
 }
 
