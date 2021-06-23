1bcc06cf1d8cd83afc7d2ad1136c78aff7073644
Layo Kaminski <layokaminski@gmail.com>
2021-06-23 19:00:00 -0300

    Exercicio 3

--- bloco_26/dia_4/index.js
+++ bloco_26/dia_4/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const MINIMUM_AGE = 17;
+
+const HTTP_UNAUTHORIZED = 401; 
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -13,6 +17,16 @@ app.post('/hello', (req, res) => {
   const { name } = req.body;
 
   res.status(200).json({ message: `Hello, ${name}` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  
+  if (Number(age) > MINIMUM_AGE) {
+    return res.status(200).json({ message: `Hello, ${name}` });
+  }
+
+  res.status(HTTP_UNAUTHORIZED).json({ message: 'Unauthorized' });
 });
 
 app.listen(3000, () => {
