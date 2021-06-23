319dafab0c6569be83b7eb6c363d83a7c0590b2c
Layo Kaminski <layokaminski@gmail.com>
2021-06-23 18:48:31 -0300

    Exercicios 1 e 2

---
+++ bloco_26/dia_4/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+
+  res.status(200).json({ message: `Hello, ${name}` });
+});
+
+app.listen(3000, () => {
+  console.log('Aplicação na porta 3000');
+});
