740b253b51c395741cda67fc699529af35c47dde
Layo Kaminski <layokaminski@gmail.com>
2021-06-23 21:18:10 -0300

    ajustes de verificação de falhas

--- bloco_26/dia_4/index.js
+++ bloco_26/dia_4/index.js
@@ -22,15 +22,15 @@ app.post('/hello', (req, res) => {
 app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
   
-  if (Number(age) > MINIMUM_AGE) {
-    return res.status(200).json({ message: `Hello, ${name}` });
+  if (Number(age) <= MINIMUM_AGE) {
+    return res.status(HTTP_UNAUTHORIZED).json({ message: 'Unauthorized' });
   }
-
-  res.status(HTTP_UNAUTHORIZED).json({ message: 'Unauthorized' });
+  res.status(200).json({ message: `Hello, ${name}` });
 });
 
 app.put('/users/:name/:age', (req, res) => {
   const { name, age } = req.params;
+
   res
     .status(200)
     .json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
