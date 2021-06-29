6e30861db63789737fd4dfbc5a37a7a5adeb8c55
Layo Kaminski <layokaminski@gmail.com>
2021-06-29 16:11:46 -0300

    Exemplo de criação de model para mysql, con node/express

---
+++ bloco_27/dia_1/model-example/index.js
@@ -0,0 +1,17 @@
+// index.js
+const express = require('express');
+const Author = require('./models/Author');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/authors', async (_req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
+
+app.listen(PORT, () => {
+    console.log(`Ouvindo a porta ${PORT}`);
+});
