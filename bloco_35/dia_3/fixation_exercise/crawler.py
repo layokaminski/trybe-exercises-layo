0f10fd2d06ffc49597dbaf65d6a05a93a060c7f5
Layo Kaminski <layokaminski@gmail.com>
2021-10-14 16:05:25 -0300

    Atualização do crawler e utilizando o arquivo teste para ajustar o valor do livro

--- bloco_35/dia_3/fixation_exercise/crawler.py
+++ bloco_35/dia_3/fixation_exercise/crawler.py
@@ -14,5 +14,20 @@ while next_page_url:
         title = product.css("h3 a::attr(title)").get()
         price = product.css(".price_color::text").get()
         print(title, price)
+
+        # Busca o detalhe de um produto
+        detail_href = product.css("h3 a::attr(href)").get()
+        detail_page_url = URL_BASE + detail_href
+
+        # Baixa o conteúdo da página de detalhes
+        detail_response = requests.get(detail_page_url)
+        detail_selector = Selector(text=detail_response.text)
+
+        # Extrai a descrição do produto
+        description = (
+          detail_selector.css("#product_description ~ p::text").get()
+          )
+        print(description)
+
     # Descobre qual é a próxima página
-    next_page_url = selector.css(".next a::attr(href)").get()
+    # next_page_url = selector.css(".next a::attr(href)").get()
--- bloco_35/dia_3/fixation_exercise/teste.py
+++ bloco_35/dia_3/fixation_exercise/teste.py
@@ -1,19 +1,9 @@
 from parsel import Selector
 import requests
 
-URL_BASE = "http://books.toscrape.com/catalogue/"
 
-response = requests.get(URL_BASE + "page-1.html")
+response = requests.get("http://books.toscrape.com/")
 selector = Selector(text=response.text)
-
-href = selector.css(".product_pod h3 a::attr(href)").get()
-detail_page_url = URL_BASE + href
-
-# baixa o conteúdo da página de detalhes
-detail_response = requests.get(detail_page_url)
-detail_selector = Selector(text=detail_response.text)
-
-# recupera a descrição do produto
-# ~ significa a tag irmã
-description = detail_selector.css("#product_description ~ p::text").get()
-print(description)
+# Extrai todos os preços da primeira página
+prices = selector.css(".product_price .price_color::text").re(r"£\d+\.\d{2}")
+print(prices)
