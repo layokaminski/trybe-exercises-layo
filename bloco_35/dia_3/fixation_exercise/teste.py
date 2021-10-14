4a89c2712b768401e4b807e2e576638f10f1c266
Layo Kaminski <layokaminski@gmail.com>
2021-10-14 16:06:25 -0300

    Testando a retirada do sufixo ...more

--- bloco_35/dia_3/fixation_exercise/teste.py
+++ bloco_35/dia_3/fixation_exercise/teste.py
@@ -2,8 +2,15 @@ from parsel import Selector
 import requests
 
 
-response = requests.get("http://books.toscrape.com/")
+response = requests.get("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
 selector = Selector(text=response.text)
-# Extrai todos os preços da primeira página
-prices = selector.css(".product_price .price_color::text").re(r"£\d+\.\d{2}")
-print(prices)
+
+# Extrai a descrição
+description = selector.css("#product_description ~ p::text").get()
+print(description)
+
+# "Fatiamos" a descrição removendo o sufixo
+suffix = "...more"
+if description.endswith(suffix):
+    description = description[:-len(suffix)]
+print(description)
