adaff0370e06b5bc37ebccf50a677d3d51c7f661
Layo Kaminski <layokaminski@gmail.com>
2021-10-22 15:22:40 -0300

    Adicionado soluções com iteratividade e recursividade

--- bloco_36/dia_2/reverse_list.py
+++ bloco_36/dia_2/reverse_list.py
@@ -4,3 +4,22 @@ def reverse_list(lista):
 
 
 print(reverse_list([2, 3, 5, 7]))
+
+
+# Iterativo
+def reverse(list):
+    reversed_list = []
+    for item in list:
+        reversed_list.insert(0, item)
+    return reversed_list
+
+
+print(reverse([2, 3, 5, 7]))
+
+
+# Recursiva
+def reverse(list):
+    if len(list) < 2:
+        return list
+    else:
+        return reverse(list[1:]) + [list[0]]
