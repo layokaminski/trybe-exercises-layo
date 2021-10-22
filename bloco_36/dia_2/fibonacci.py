16314808111ed6c82fd576a0fc70c2005fb867db
Layo Kaminski <layokaminski@gmail.com>
2021-10-22 15:09:50 -0300

    Colocando a função de fibonacci com recursividade

--- bloco_36/dia_2/fibonacci.py
+++ bloco_36/dia_2/fibonacci.py
@@ -1,10 +1,18 @@
-def fibonacci(number):
+def fibonacci_sem_recursividade(number):
     array = [0, 1]
 
     while len(array) < number:
         append_number = array[-1] + array[-2]
         array.append(append_number)
     return array
+
+
+# Com recursividade
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 print(fibonacci(10))
