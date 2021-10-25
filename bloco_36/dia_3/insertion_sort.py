283cf6f5dc52a090c4f1a114fd873b9a09e40964
Layo Kaminski <layokaminski@gmail.com>
2021-10-25 14:49:04 -0300

    Criando algoritmo de ordenação por inserção

---
+++ bloco_36/dia_3/insertion_sort.py
@@ -0,0 +1,19 @@
+def insertion_sort(array):
+    # itera sobre cada valor do array
+    for i in range(len(array)):
+        current_value = array[i]
+        current_position = i
+        # enquanto o valor da posição for menor que os elementos a sua esquerda
+        while (
+            current_position > 0
+            and array[current_position - 1] > current_value
+        ):
+            # move as posições para a direita
+            array[current_position] = array[current_position - 1]
+            current_position = current_position - 1
+        # colocamos o elemento em sua nova posição
+        array[current_position] = current_value
+    return array
+
+
+print(insertion_sort([100, 4, 6, 33, 56, 67]))
