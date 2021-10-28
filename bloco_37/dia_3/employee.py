4f9de19d30f7aa0d0f6850f79b07cfb3a7077405
Layo Kaminski <layokaminski@gmail.com>
2021-10-28 16:16:04 -0300

    Atualizando a lista para ocorrer um separate chaining

--- bloco_37/dia_3/employee.py
+++ bloco_37/dia_3/employee.py
@@ -6,18 +6,21 @@ class Employee:
 
 class HashMap:
     def __init__(self):
-        self._buckets = [None for i in range(10)]
+        self._buckets = [[] for i in range(10)]
 
     def get_address(self, id_num):
         return id_num % 10
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
-        self._buckets[address] = employee
+        self._buckets[address].append(employee)
 
     def get_value(self, id_num):
         address = self.get_address(id_num)
-        return self._buckets[address].name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
 
     def has(self, id_num):
         address = self.get_address(id_num)
